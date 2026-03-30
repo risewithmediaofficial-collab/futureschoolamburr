6b15c9ad640d224cde7a7abe79f5bb602ad0c42d
sathish <sathishyadav8898@gmail.com>
2026-03-30 11:57:15 +0530

    Admin API URL updated for production deployment.

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:3000/api' 
+  : '/api'
 
 // Create axios instance
 const apiClient = axios.create({
